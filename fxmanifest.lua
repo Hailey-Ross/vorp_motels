37b00027431f6c919e1eee02a99b74e83f0b32f2
Agent Colorado <26784904+LordBobRoss@users.noreply.github.com>
2022-02-23 14:21:42 -0700

    Conversion
    Converting to GhmattiMysql and VORP-Core

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,8 +2,16 @@ fx_version "adamant"
 games {"rdr3"}
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
-server_script {"@mysql-async/lib/MySQL.lua"}
-client_script {"@redm-rpc/lib.lua"}
-server_script {"@redm-rpc/lib.lua"}
-client_script {'client/cl_*.lua'}
-server_script {'server/sv_*.lua'}
+dependency "ghmattimysql"
+dependency "vorp_core"
+
+server_scripts {
+	"server/sv_main.lua",
+	"server/sv_room_config.lua.lua"
+}
+
+client_scripts {
+	"client/cl_main.lua",
+	"client/cl_utils.lua",
+  "client/cl_commands.lua"
+}
